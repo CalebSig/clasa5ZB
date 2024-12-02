310d3f869dc879f66c62b25d6e68f7a8d51257e3
CalebSig <Sighiartau.Do.Achim@student.utcluj.ro>
2024-12-02 08:55:52 +0200

    Update DeterminareaDivizorilor.cpp

--- DeterminareaDivizorilor.cpp
+++ DeterminareaDivizorilor.cpp
@@ -6,11 +6,11 @@ int main()
 {
     int n;
     cin>>n;
-    for(int d=2;d*d<=n;d++)
+    for(int d=1;d*d<=n;d++)
         if(n%d==0)
         {
             //... lucram cu diviziorul d
-            // if(d*d!=n) //... lucram cu div n/d
+            // if(d*d!=n) //... lucram cu div n/d DACA NU AM AJUNS LA RADICAL!
         }
     return 0;
 }
